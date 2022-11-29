f16d5b52cf53135e975a5027d06609f547ab6d82
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2022-11-29 11:52:03 +0100

    added dailyservice to monthly

--- src/main/java/com/example/backend/MonthlyBudget/Controller/MonthlyBudgetController.java
+++ src/main/java/com/example/backend/MonthlyBudget/Controller/MonthlyBudgetController.java
@@ -1,10 +1,13 @@
 package com.example.backend.MonthlyBudget.Controller;
 
+import com.example.backend.DailyBudget.Model.DailyBudget;
+import com.example.backend.DailyBudget.Service.DailyBudgetService;
 import com.example.backend.MonthlyBudget.Model.MonthlyBudget;
 import com.example.backend.MonthlyBudget.Service.MonthlybudgetService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Iterator;
 import java.util.Optional;
 
 @CrossOrigin
@@ -13,11 +16,14 @@ import java.util.Optional;
 public class MonthlyBudgetController {
 
     private final MonthlybudgetService service;
+    private final DailyBudgetService dailyService;
+
+    public MonthlyBudgetController(MonthlybudgetService service, DailyBudgetService dailyService){
+        this.dailyService = dailyService;
+        this.service = service;}
 
     @GetMapping
     public Iterable<MonthlyBudget> callApi(){return service.findAll();}
-
-    public MonthlyBudgetController(MonthlybudgetService service){this.service = service;}
 
     @GetMapping("/{id}")
     public ResponseEntity<MonthlyBudget> find(@PathVariable("id") Long id){
@@ -27,13 +33,17 @@ public class MonthlyBudgetController {
     }
     @PostMapping()
     public ResponseEntity<MonthlyBudget> create(@RequestBody MonthlyBudget monthlyBudget) {
+        Iterator newIte = monthlyBudget.getDailyBudgets().iterator();
+        while(newIte.hasNext()) dailyService.create((DailyBudget) newIte.next());
         MonthlyBudget budgetItem = service.create(monthlyBudget);
         return ResponseEntity.ok().body(budgetItem);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<MonthlyBudget> patch(@PathVariable("id") Long id,
+    public ResponseEntity<MonthlyBudget> put(@PathVariable("id") Long id,
                                                @RequestBody MonthlyBudget monthlyBudget){
+        Iterator newIte = monthlyBudget.getDailyBudgets().iterator();
+        while(newIte.hasNext()) dailyService.create((DailyBudget) newIte.next());
         return ResponseEntity.ok().body(service.update(id, monthlyBudget));
     }
     @DeleteMapping("/{id}")
@@ -42,5 +52,18 @@ public class MonthlyBudgetController {
         MonthlyBudget monthlyBudget = service.delete(id);
         return ResponseEntity.ok().body(monthlyBudget);
     }
+
+//    @PatchMapping("/{id}")
+//    public ResponseEntity<MonthlyBudget> patch(@PathVariable("id") Long id,
+//                                               @RequestBody MonthlyBudget monthlyBudget) {
+//            Optional<MonthlyBudget> extantMonthlyBudget = Optional.of(service.find(id)
+//                .orElseThrow(() -> new RuntimeException("Monthly Budget not found for Patch.".formatted(id))));
+//            extantMonthlyBudget.get(monthlyMoney);
+            //2811 2022 jeg vil hente budgettet og se på hvad for værdier det har
+            //            if (extantMonthlyBudget.isPresent()) {
+//                MonthlyBudget newBudg = new MonthlyBudget();
+//                newBudg = extantMonthlyBudget;
+//            }
+//    }
 
 }
