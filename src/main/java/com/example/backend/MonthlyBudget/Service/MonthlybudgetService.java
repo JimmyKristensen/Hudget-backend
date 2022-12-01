2a39a5f1c921cac4f8e5c4ad5549707223fdc96d
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2022-12-01 14:26:58 +0100

    added DailyService connection and createNewDailyBudgets method. Fixed update.

--- src/main/java/com/example/backend/MonthlyBudget/Service/MonthlybudgetService.java
+++ src/main/java/com/example/backend/MonthlyBudget/Service/MonthlybudgetService.java
@@ -1,18 +1,24 @@
 package com.example.backend.MonthlyBudget.Service;
 
 
+import com.example.backend.DailyBudget.Model.DailyBudget;
+import com.example.backend.DailyBudget.Service.DailyBudgetService;
 import com.example.backend.MonthlyBudget.Model.MonthlyBudget;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.Month;
+import java.util.Iterator;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class MonthlybudgetService {
     private final CrudRepository<MonthlyBudget, Long> repository;
+    private final DailyBudgetService dailyService;
 
-    public MonthlybudgetService(CrudRepository<MonthlyBudget,Long> repository){this.repository = repository;}
+    public MonthlybudgetService(CrudRepository<MonthlyBudget,Long> repository, DailyBudgetService dailyService) {
+        this.repository = repository;
+        this.dailyService = dailyService;}
 
     public Iterable<MonthlyBudget> findAll(){return repository.findAll();}
 
@@ -23,11 +29,19 @@ public class MonthlybudgetService {
     public MonthlyBudget create(MonthlyBudget monthlyBudget) {
         return repository.save(monthlyBudget);
     }
-    public MonthlyBudget update(Long id, MonthlyBudget monthlyBudget) { return repository.save(monthlyBudget);}
+    public Optional<MonthlyBudget> update(Long id, MonthlyBudget monthlyBudget) {
+        return repository.findById(id)
+                .map(oldItem -> repository.save(oldItem.updateWith(monthlyBudget)));
+    }
 
     public MonthlyBudget delete(long id){
         repository.deleteById(id);
         return null;
     }
+    public void createNewDailyBudgets(Set<DailyBudget> dailyBudgets) {
+        Iterator newIte = dailyBudgets.iterator();
+        while(newIte.hasNext()) dailyService.create((DailyBudget) newIte.next());
+    }
+
 
 }
