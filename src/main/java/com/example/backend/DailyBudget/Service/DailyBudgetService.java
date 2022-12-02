e4522d398cb857f2613692b65ac348ae02da0e32
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2022-12-02 13:02:48 +0100

    removed static modifiers from dailybudgetservice and added expensesservice to dbs

--- src/main/java/com/example/backend/DailyBudget/Service/DailyBudgetService.java
+++ src/main/java/com/example/backend/DailyBudget/Service/DailyBudgetService.java
@@ -1,32 +1,30 @@
 package com.example.backend.DailyBudget.Service;
 
-import com.example.backend.AdditionalExpenses.Model.AdditionalExpenses;
 import com.example.backend.AdditionalExpenses.Service.ExpensesService;
 import com.example.backend.DailyBudget.Model.DailyBudget;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.YearMonth;
-import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class DailyBudgetService {
 
-    static List<DailyBudget> days = new ArrayList<>();
-    static List<DailyBudget> setDays = new ArrayList<>();
+      private Set<DailyBudget> days;
+      private Set<DailyBudget> setDays;
+      private ExpensesService expService;
 
 
     //class field
     private final CrudRepository<DailyBudget, Long> repository;
 
     //constructor
-    public DailyBudgetService(CrudRepository<DailyBudget, Long> repository) {this.repository = repository; }
+    public DailyBudgetService(CrudRepository<DailyBudget, Long> repository, ExpensesService expService) {
+        this.repository = repository;
+        this.expService = expService;
+    }
 
     public Iterable<DailyBudget> findAll(){return repository.findAll();}
 
@@ -34,7 +32,9 @@ public class DailyBudgetService {
         return repository.findById(id);
     }
 
-    public List<DailyBudget> create(){
+    public Set<DailyBudget> create(){
+        days = new HashSet<>();
+          setDays = new HashSet<>();
 
         //We find todays date, and the end of the months date
         LocalDate today = LocalDate.now();
@@ -45,15 +45,15 @@ public class DailyBudgetService {
         String[] currentDay = today.toString().split("-");
         String[] lastDay = end.toString().split("-");
         //We convert the last part to a Integer
-        Integer daysStart = Integer.parseInt(currentDay[2]);
-        Integer daysEnd = Integer.parseInt(lastDay[2]);
+        int daysStart = Integer.parseInt(currentDay[2]);
+        int daysEnd = Integer.parseInt(lastDay[2]);
 
         //Days left in the month
-        Integer daysBetween = daysEnd-daysStart;
+        int daysBetween = daysEnd-daysStart;
 
         //Make enteties depending on how many days
         for (int i = 0; i < daysBetween+1; i++) {
-            DailyBudget newDailyBudget = new DailyBudget(0, today, ExpensesService.returnDailySet());
+            DailyBudget newDailyBudget = new DailyBudget(0, today, expService.returnDailySet());
             setDays.add(newDailyBudget);
             today = today.plusDays(1);
             days.add(repository.save(newDailyBudget));
@@ -64,6 +64,5 @@ public class DailyBudgetService {
     public DailyBudget update(Long id, DailyBudget dailyBudget){
         return repository.save(dailyBudget);
     }
-
 
 }
