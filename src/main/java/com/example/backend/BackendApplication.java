d877e518105d700fc878b5b2f33c900004d689f4
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2022-12-05 15:18:31 +0100

    minor refactor

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -8,7 +8,6 @@ import com.example.backend.MonthlyBudget.Model.MonthlyBudget;
 import com.example.backend.MonthlyBudget.Repository.MonthlyBudgetRepository;
 import com.example.backend.User.Model.User;
 import com.example.backend.User.Repository.UserRepository;
-import org.hibernate.type.descriptor.java.LocalDateJavaDescriptor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
@@ -16,14 +15,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.temporal.TemporalAdjusters;
 import java.util.*;
 
 @SpringBootApplication
 public class BackendApplication {
-
-    Date date;
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
@@ -37,42 +32,38 @@ public class BackendApplication {
     )
     { return (args) ->{
 
-
         //Diffrent HashSet, to fulfill the oneToMany requirement asked in the entities
         Set<DailyBudget> dailySet = new HashSet<DailyBudget>();
         Set<DailyBudget> dailySetEmpty = new HashSet<DailyBudget>();
         Set<AdditionalExpenses> additionalExpensesSet = new HashSet<>();
         LocalDate localDate = LocalDate.now();
 
-
         LocalDate now = LocalDate.now(); //tilføjet denne så vi kan give Budget-construc "now" istedet for "LocalDate.now()";
-
+        //bliver til yyyy-mm-dd
 
         final List<AdditionalExpenses> expensesList = new ArrayList<>();
         expensesList.add(new AdditionalExpenses());
         expensesList.add(new AdditionalExpenses());
-        expenses.saveAll(expensesList);
-
-
+     //   expenses.saveAll(expensesList);
 
         final List<DailyBudget> dailybudgets = new ArrayList<>();
         dailybudgets.add(new DailyBudget(266, now,additionalExpensesSet));
         dailybudgets.add(new DailyBudget(223, now,additionalExpensesSet));
         dailybudgets.add(new DailyBudget(245, now,additionalExpensesSet));
 
-        dailyBudget.saveAll(dailybudgets);
+   //     dailyBudget.saveAll(dailybudgets);
         for (DailyBudget bd : dailybudgets) {
             dailySet.add(bd);
         }
 
+        List<MonthlyBudget> monthlyBudgetsList = new ArrayList<>();
+        monthlyBudgetsList.add(new MonthlyBudget(2500, now.toString()));
+     //   monthlyBudget.saveAll(monthlyBudgetsList); //testkode -- alle kaldene til saveAll er udkommenteret.
 
         final List<User> users = new ArrayList<>();
         users.add(new User("Timmie","jegErSuperSej"));
         users.add(new User("Hans","kodeord13"));
         login.saveAll(users);
         System.out.println("CommandLineRunner has completed setup -- Hudget ready to run.");
-
     };}
-
-
 }
