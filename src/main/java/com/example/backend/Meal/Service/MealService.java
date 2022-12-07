3b7b413d7be379bdd25a1850b851e67bd8fce817
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2022-12-07 15:00:56 +0100

    updated DefaultMealInitialiser

--- src/main/java/com/example/backend/Meal/Service/MealService.java
+++ src/main/java/com/example/backend/Meal/Service/MealService.java
@@ -4,7 +4,9 @@ import com.example.backend.Meal.Model.Meal;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class MealService {
@@ -18,9 +20,21 @@ public class MealService {
     public Meal create(Meal meal){return repository.save(meal);}
     public Meal update(Long id, Meal meal){
         return repository.save(meal);
-    }
+    } //BUG: cannot patch, only put.
     public Meal delete(Long id) {
         repository.deleteById(id);
         return null;
+    }
+
+    public Set<Meal> defaultMealInitialiser() { //default "shape" of Meals for a user.
+        String[] mealNameStringArray = {"breakfast", "midday", "afternoon"}; //names for the meals.
+        Set<Meal> mealSet = new HashSet<>();//init set
+        Meal meal; //init meal
+        for (String str : mealNameStringArray) { //for-each loops through the array -- once for each string in the array
+            meal = new Meal(str,  33.0f); //creates the meal, using str as the name
+            mealSet.add(meal); //adds the meal to the set
+        }
+        for (Meal m : mealSet) create(m); //saves the meals to the database
+        return mealSet; //returns the meal-set to the User-constructor
     }
 }
