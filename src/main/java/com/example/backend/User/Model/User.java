c1fd6a48f108054910366d182ff3cfbeda1196f3
Hans Victor Munck-Westh <durandal2nd@gmail.com>
2022-12-08 11:56:26 +0100

    removed meal from user (meal is in Daily)

--- src/main/java/com/example/backend/User/Model/User.java
+++ src/main/java/com/example/backend/User/Model/User.java
@@ -1,12 +1,10 @@
 package com.example.backend.User.Model;
 
-import com.example.backend.Meal.Model.Meal;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -21,15 +19,7 @@ public class User {
     private String name;
     private String password;
 
-    @OneToMany //one user has a number of meals
-    private Set<Meal> userMeal;
-
-    public User(String name, String password, Set<Meal> meals) {
-        this.name = name;
-        this.password = password;
-        this.userMeal = meals;
-    }
-    public User(String name, String password) { //0712 2022 denne konstru bruges kun af CmdLineRunnerens test-cases
+    public User(String name, String password) {
         this.name = name;
         this.password = password;
     }
